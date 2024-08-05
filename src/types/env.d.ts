bb6e2f53ea0a73a8800e83ff6b66d5ad0dfdd9d4
sergey.krasnov <sergey.krasnov@truelab.games>
2024-08-05 16:37:02 +0300

    feat: docker build

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,13 +4,13 @@ declare global {
     interface ProcessEnv {
       PORT: string;
       NODE_ENV: 'production' | 'development' | 'staging' | 'testing';
-      DB_NAME: string;
-      DB_USER: string;
-      DB_PASSWORD: string;
-      DB_HOST: string;
-      DB_PORT: string;
       WHITE_LIST: string;
       JWT_SECRET_KEY: string;
+      POSTGRES_DB: string;
+      POSTGRES_USER: string;
+      POSTGRES_PASSWORD: string;
+      POSTGRES_HOST: string;
+      POSTGRES_PORT: string;
     }
   }
 }
