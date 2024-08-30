0e0055b7c5ef150f1d6c14d7a02b4a506a7a0c37
sergey.krasnov <sergey.krasnov@truelab.games>
2024-08-30 19:00:38 +0300

    fix: log

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import process from 'node:process';
 
 import { MainController } from './app';
@@ -18,6 +19,16 @@ const {
 
 // eslint-disable-next-line no-void, @typescript-eslint/explicit-function-return-type
 void (async function main() {
+  console.log('PORT', PORT);
+  console.log('NODE_ENV', NODE_ENV);
+  console.log('WHITE_LIST', WHITE_LIST);
+  console.log('JWT_SECRET_KEY', JWT_SECRET_KEY);
+  console.log('POSTGRES_DB', POSTGRES_DB);
+  console.log('POSTGRES_USER', POSTGRES_USER);
+  console.log('POSTGRES_PASSWORD', POSTGRES_PASSWORD);
+  console.log('POSTGRES_HOST', POSTGRES_HOST);
+  console.log('POSTGRES_PORT', POSTGRES_PORT);
+
   const server = new Server({
     port: Number(PORT),
   });
